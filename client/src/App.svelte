0fa3cf0b14dc0556e35d1e56ababc4aaa5b1a69d
ryantran <113406848+rryantran@users.noreply.github.com>
2025-05-13 23:42:42 -0500

    add file upload (no drag and drop)

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,8 +1,10 @@
 <script>
   import ThresholdSlider from "./lib/ThresholdSlider.svelte";
+  import FileUpload from "./lib/FileUpload.svelte";
 </script>
 
 <main class="flex flex-col items-center justify-evenly min-h-screen">
   <h1 class="text-3xl">jillustrate</h1>
+  <FileUpload />
   <ThresholdSlider />
 </main>
---
+++ client/src/lib/FileUpload.svelte
@@ -0,0 +1,26 @@
+<script>
+  let fileInput;
+
+  function openFileBrowser() {
+    fileInput.click();
+  }
+
+  function handleDragOver(e) {
+    e.preventDefault();
+  }
+
+  function handleDrop(e) {
+    e.preventDefault();
+  }
+</script>
+
+<button
+  class="cursor-pointer border-2 border-dashed rounded-lg p-5 hover:bg-zinc-100"
+  on:click={openFileBrowser}
+  on:dragover={handleDragOver}
+  on:drop={handleDrop}
+>
+  <p>Drag and drop a file</p>
+  <p>or click to select one</p>
+  <input type="file" accept="image/png" bind:this={fileInput} hidden />
+</button>
